4abc6b590e0faa4057c04e4388247f24a2e377bc
Roxulu67 <rodrigogt03@gmail.com>
2025-02-12 13:23:21 -0600

    Main text added

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -18,10 +18,45 @@ class _HomePageState extends State<HomePage> {
         Icons.menu,
         color: Colors.grey[800]
         ),
-      actions:[ 
-        Icon(Icons.person)
+      actions: const [ 
+        Padding(
+          padding: EdgeInsets.only(right: 24.0),
+          child: Icon(Icons.person),
+        )
         ],
-      )
+      ),
+      body:const Column(
+        children: [
+          //Texto principal
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal:36, vertical:18),
+            child: Row(
+              children: [
+                Text(
+                  "I want to ",
+                  style: TextStyle(fontSize:32), 
+                ),
+                Text ("Eat",
+                  style: TextStyle(
+                    //Tamaño de letra
+                    fontSize: 32,
+                    //Negritas
+                    fontWeight: FontWeight.bold,
+                    //Subrayado
+                    decoration: TextDecoration.underline))
+              
+              ],
+            ),
+          ),
+      
+          //Pestañas (TabBar)
+
+          //Contenido de pestañas (TabBarView)
+
+          //Carrito (Cart)
+
+        
+        ],)
     );
   }
 }
