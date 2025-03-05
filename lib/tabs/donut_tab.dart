4476ebb03fc232ee2494c99fc7020cc1a77dc4dc
Roxulu67 <rodrigogt03@gmail.com>
2025-03-05 13:32:16 -0600

    PriceTag Added

--- lib/tabs/donut_tab.dart
+++ lib/tabs/donut_tab.dart
@@ -1,40 +1,40 @@
+import 'package:donut_app_2c_tovar/utils/donut_tile.dart';
 import 'package:flutter/material.dart';
+
 class DonutTab extends StatelessWidget {
 
   //Lista de donas
   final List donutsOnSale = [
-   // [ donutFlavor, donutPrice, donutColor, imageName ]
+    // [ donutFlavor, donutPrice, donutColor, imageName ]
    ["Ice Cream", "36", Colors.blue, "lib/images/icecream_donut.png"],
    ["Strawberry", "45", Colors.red, "lib/images/strawberry_donut.png"],
    ["Grape Ape", "84", Colors.purple, "lib/images/grape_donut.png"],
    ["Choco", "95", Colors.brown, "lib/images/chocolate_donut.png"],
-  ]; 
-   DonutTab ({super.key});
-    
+
+  ];
+  DonutTab ({super.key});
+
   @override
   Widget build(BuildContext context) {
-    //Widget para usar cuadrícula
     return GridView.builder(
-     //Cuántos elementos hay
-     itemCount: donutsOnSale.length,
-     padding: EdgeInsets.all(12),
-     //Prepa 1.Cómo se distribuirán los elementos
-     gridDelegate: 
-     SliverGridDelegateWithFixedCrossAxisCount(
-          //Cuántas columnas
-          crossAxisCount: 2
-
-       ),
-       itemBuilder: (context,index){
-         //Cada elemento individual
-         return DonutTile(
-             donutFlavor: donutsOnSale[index][0],
-             donutPrice: donutsOnSale[index][1],
-             donutColor: donutsOnSale[index][2],
-             imageName: donutsOnSale[index][3],
-      );
-    }
-  )
+      //Cuantos elementos tiene
+      itemCount: donutsOnSale.length,
+      padding: const EdgeInsets.all(12),
+      //Encargado de organizar la cuadrícula
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        //Determina el número de columnas
+        crossAxisCount: 2,
+        //Relación de aspecto
+        childAspectRatio: 1 / 1.5),
+      itemBuilder: (context, index){
+        //Elemento individual de la cuadrícula
+        return DonutTile(
+          donutFlavor: donutsOnSale[index][0],
+          donutPrice: donutsOnSale[index][1],
+          donutColor: donutsOnSale[index][2],
+          imageName: donutsOnSale[index][3],
+        );
+      });
   }
 }
 
---
+++ lib/utils/donut_tile.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+class DonutTile extends StatelessWidget {
+ final String donutFlavor;
+ final String donutPrice;
+ final dynamic donutColor;  //dynamic porque será de tipo Color y también usará []
+ final String imageName;
+
+ const DonutTile({
+   super.key,
+   required this.donutFlavor,
+   required this.donutPrice,
+   required this.donutColor,
+   required this.imageName,
+ });
+
+ @override
+ Widget build(BuildContext context) {
+  return Padding(
+     padding: const EdgeInsets.all(8.0),
+     child: Container(
+         decoration: BoxDecoration(
+              color: donutColor[50],
+            borderRadius: BorderRadius.circular(24)),
+          child: Column(
+            children: [
+              //Price Tag
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      color: donutColor[100],
+                      borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(24),
+                        topRight: Radius.circular(24),
+                      )),
+                    padding:
+                      const EdgeInsets.symmetric(vertical: 8, horizontal: 18),
+                    child: Text(
+                      '\$$donutPrice', 
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold, 
+                        fontSize: 18, 
+                        color: donutColor[800])),
+                  )
+                ],
+              ),
+              //Donut Picture
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 24.0,
+                  vertical: 12
+                ),
+                child: Image.asset(imageName),
+              )
+
+              //Donut Flavor
+
+              //Love icon + add buton
+
+            ],
+          ),
+            ),
+   );
+ }
+}
