a29fbe8d44b89863b7a045655aa313e523d1e6ee
Roxulu67 <rodrigogt03@gmail.com>
2025-03-09 13:18:43 -0600

    DonutTile Finished

--- lib/tabs/donut_tab.dart
+++ lib/tabs/donut_tab.dart
@@ -5,11 +5,11 @@ class DonutTab extends StatelessWidget {
 
   //Lista de donas
   final List donutsOnSale = [
-    // [ donutFlavor, donutPrice, donutColor, imageName ]
-   ["Ice Cream", "36", Colors.blue, "lib/images/icecream_donut.png"],
-   ["Strawberry", "45", Colors.red, "lib/images/strawberry_donut.png"],
-   ["Grape Ape", "84", Colors.purple, "lib/images/grape_donut.png"],
-   ["Choco", "95", Colors.brown, "lib/images/chocolate_donut.png"],
+    // [ donutFlavor, donutStore, donutPrice, donutColor, imageName ]
+   ["Ice Cream", "Krispy Kreme", "36", Colors.blue, "lib/images/icecream_donut.png"],
+   ["Strawberry", "Dunkin donuts", "45", Colors.red, "lib/images/strawberry_donut.png"],
+   ["Grape Ape", "Aurrerá", "84", Colors.purple, "lib/images/grape_donut.png"],
+   ["Choco", "Costco", "95", Colors.brown, "lib/images/chocolate_donut.png"],
 
   ];
   DonutTab ({super.key});
@@ -30,11 +30,15 @@ class DonutTab extends StatelessWidget {
         //Elemento individual de la cuadrícula
         return DonutTile(
           donutFlavor: donutsOnSale[index][0],
-          donutPrice: donutsOnSale[index][1],
-          donutColor: donutsOnSale[index][2],
-          imageName: donutsOnSale[index][3],
+          donutStore: donutsOnSale[index][1],
+          donutPrice: donutsOnSale[index][2],
+          donutColor: donutsOnSale[index][3],
+          imageName: donutsOnSale[index][4],
         );
-      });
+
+
+      },
+    );
   }
 }
 
--- lib/utils/donut_tile.dart
+++ lib/utils/donut_tile.dart
@@ -5,13 +5,15 @@ class DonutTile extends StatelessWidget {
  final String donutPrice;
  final dynamic donutColor;  //dynamic porque será de tipo Color y también usará []
  final String imageName;
+ final String donutStore;
 
  const DonutTile({
    super.key,
    required this.donutFlavor,
    required this.donutPrice,
    required this.donutColor,
-   required this.imageName,
+   required this.imageName, 
+   required this.donutStore,
  });
 
  @override
@@ -42,7 +44,7 @@ class DonutTile extends StatelessWidget {
                       style: TextStyle(
                         fontWeight: FontWeight.bold, 
                         fontSize: 18, 
-                        color: donutColor[800])),
+                        color: donutColor[80])),
                   )
                 ],
               ),
@@ -53,15 +55,38 @@ class DonutTile extends StatelessWidget {
                   vertical: 12
                 ),
                 child: Image.asset(imageName),
-              )
+              ),
 
               //Donut Flavor
-
-              //Love icon + add buton
-
-            ],
-          ),
-            ),
+              Text(donutFlavor,
+                  style: 
+                      const TextStyle(fontWeight:FontWeight.bold, fontSize: 24)),
+              //Espacio entre textos
+              const SizedBox(
+                height: 4,
+              ),
+              //Donut Store
+              Text(donutStore,
+                style: TextStyle(
+                    fontWeight: FontWeight.normal,
+                    fontSize: 16,
+                    color: Colors.grey[600])),
+              //Espacio entre textos
+              const SizedBox(height: 4),
+              //Love icon + add button
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: 
+                  MainAxisAlignment.spaceBetween,
+                  children: [
+                    Icon(Icons.favorite_border),
+                    Icon(Icons.add)
+                  ],
+                ),
+              )
+      ],),
+    ),
    );
  }
 }
